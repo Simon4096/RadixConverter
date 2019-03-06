7b3faf798b8ccca5340ddc38aa0e161171ece372
Xuan <dim@no.where>
2019-03-05 23:26:16 -0800

    replace tab with space

--- RadixConverter.h
+++ RadixConverter.h
@@ -6,36 +6,36 @@
 
 namespace cxpl::RadixConverter
 {
-	std::vector<int> Convert(std::vector<int> in, int from, int to)
-	{
-		std::vector<int> out;
-		auto BigIntegerMod = [](std::vector<int> &num, int radix, int divisor, int &remainder)
-		{
-			int divident = 0;
-			bool isQuotientNonzero = false;
-			for (auto &n : num)
-			{
-				divident = divident * radix + n;
-				n = divident / divisor;
-				divident %= divisor;
+    std::vector<int> Convert(std::vector<int> in, int from, int to)
+    {
+        std::vector<int> out;
+        auto BigIntegerMod = [](std::vector<int> &num, int radix, int divisor, int &remainder)
+        {
+            int divident = 0;
+            bool isQuotientNonzero = false;
+            for (auto &n : num)
+            {
+                divident = divident * radix + n;
+                n = divident / divisor;
+                divident %= divisor;
 
-				isQuotientNonzero = isQuotientNonzero || n > 0;
-			}
+                isQuotientNonzero = isQuotientNonzero || n > 0;
+            }
 
-			remainder = divident;
-			return isQuotientNonzero;
-		};
+            remainder = divident;
+            return isQuotientNonzero;
+        };
 
-		int remainder = 0;
-		while (BigIntegerMod(in, from, to, remainder))
-		{
-			out.push_back(remainder);
-		}
+        int remainder = 0;
+        while (BigIntegerMod(in, from, to, remainder))
+        {
+            out.push_back(remainder);
+        }
 
-		out.push_back(remainder);
-		std::reverse(out.begin(), out.end());
-		return out;
-	}
+        out.push_back(remainder);
+        std::reverse(out.begin(), out.end());
+        return out;
+    }
 }
 
 
--- test/test.cpp
+++ test/test.cpp
@@ -12,52 +12,52 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace test
-{		
-	TEST_CLASS(RadixConverter)
-	{
-	public:
-		
-		TEST_METHOD(Convert)
-		{
-			// TODO: Your test code here
-			using cxpl::RadixConverter::Convert;
+{
+    TEST_CLASS(RadixConverter)
+    {
+    public:
 
-			std::default_random_engine rng;
-			std::uniform_int_distribution<int> radix_dist(2, 62);
-			auto GetRandomRadix = std::bind(radix_dist, rng);
-			std::uniform_int_distribution<int> length_dist(1, 10);
-			auto GetRandomLength = std::bind(length_dist, rng);
+        TEST_METHOD(Convert)
+        {
+            // TODO: Your test code here
+            using cxpl::RadixConverter::Convert;
 
-			for (int n = 0; n < 1000; n++)
-			{
-				// Generate input
-				int radix_from = GetRandomRadix();
-				int radix_to = GetRandomRadix();
-				int length_from = GetRandomLength();
+            std::default_random_engine rng;
+            std::uniform_int_distribution<int> radix_dist(2, 62);
+            auto GetRandomRadix = std::bind(radix_dist, rng);
+            std::uniform_int_distribution<int> length_dist(1, 10);
+            auto GetRandomLength = std::bind(length_dist, rng);
 
-				std::uniform_int_distribution<int> digit_dist(0, radix_from);
-				auto GetRandomDigit = std::bind(digit_dist, rng);
+            for (int n = 0; n < 1000; n++)
+            {
+                // Generate input
+                int radix_from = GetRandomRadix();
+                int radix_to = GetRandomRadix();
+                int length_from = GetRandomLength();
 
-				std::vector<int> in(length_from);
-				std::generate(in.begin(), in.end(), GetRandomDigit);
+                std::uniform_int_distribution<int> digit_dist(0, radix_from);
+                auto GetRandomDigit = std::bind(digit_dist, rng);
 
-				// Convert
-				std::vector<int> out = Convert(in, radix_from, radix_to);
+                std::vector<int> in(length_from);
+                std::generate(in.begin(), in.end(), GetRandomDigit);
 
-				// Validate
-				uint64_t in_value = 0, out_value = 0;
-				in_value = std::accumulate(in.begin(), in.end(), in_value, [radix_from](uint64_t value, int digit) {return value * radix_from + digit; });
-				out_value = std::accumulate(out.begin(), out.end(), out_value, [radix_to](uint64_t value, int digit) {return value * radix_to + digit; });
+                // Convert
+                std::vector<int> out = Convert(in, radix_from, radix_to);
 
-				if (in_value != out_value)
-				{
-					std::cout << "error" << std::endl;
-					auto out = Convert(in, radix_from, radix_to);
-				}
+                // Validate
+                uint64_t in_value = 0, out_value = 0;
+                in_value = std::accumulate(in.begin(), in.end(), in_value, [radix_from](uint64_t value, int digit) {return value * radix_from + digit; });
+                out_value = std::accumulate(out.begin(), out.end(), out_value, [radix_to](uint64_t value, int digit) {return value * radix_to + digit; });
 
-				Assert::AreEqual(in_value, out_value);
-			}
-		}
+                if (in_value != out_value)
+                {
+                    std::cout << "error" << std::endl;
+                    auto out = Convert(in, radix_from, radix_to);
+                }
 
-	};
+                Assert::AreEqual(in_value, out_value);
+            }
+        }
+
+    };
 }
